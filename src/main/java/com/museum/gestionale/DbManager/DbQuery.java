3d8cdcbe26ff4dcce24f3c5300c409fef58149b1
Immortalis202 <lmalegori@gmail.com>
2024-09-29 14:29:09 +0200

    Added Query Class

--- src/main/java/com/museum/gestionale/DbManager/DbQuery.java
+++ src/main/java/com/museum/gestionale/DbManager/DbQuery.java
@@ -1,17 +1,29 @@
 package com.museum.gestionale.DbManager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DbQuery {
-    private final Connection connection;
 
-    public DbQuery() throws SQLException {
-        this.connection = DbConnectionManager.getInstance().getConnection();
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DbQuery(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    public List<Map<String, Object>> executeQuery(String sql) {
+        return jdbcTemplate.queryForList(sql);
+    }
 
+    public List<Map<String, Object>> getallOpere() {
+        String sql = "SELECT * FROM opera";
+        return executeQuery(sql);
+    }
 }
