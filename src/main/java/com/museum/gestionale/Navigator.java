201d355e99a22d5d266f2b41333817f125edb778
Immortalis202 <lmalegori@gmail.com>
2024-10-09 12:25:40 +0200

    Refactor of some code

--- src/main/java/com/museum/gestionale/Navigator.java
+++ src/main/java/com/museum/gestionale/Navigator.java
@@ -1,8 +1,12 @@
 package com.museum.gestionale;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class Navigator {
@@ -16,6 +20,29 @@ public class Navigator {
     @RequestMapping("/info")
     public String operePage() {
         return "info";
+    }
+
+
+    @GetMapping("/addPainting")
+    public String showForm() {
+        return "paintingForm";
+    }
+
+    @PostMapping("/submitPainting")
+    public String submitPainting(@RequestParam String name,
+                                 @RequestParam String author,
+                                 @RequestParam String date,
+                                 @RequestParam String description,
+                                 @RequestParam String position,
+                                 @RequestParam String originalMuseum,
+                                 @RequestParam String material,
+                                 @RequestParam double height,
+                                 @RequestParam double width,
+                                 @RequestParam MultipartFile image,
+                                 Model model) {
+
+        model.addAttribute("message", "Painting submitted successfully!");
+        return "result";
     }
 
 }
