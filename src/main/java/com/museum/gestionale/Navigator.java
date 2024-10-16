c58510fd5666a84cdb0ac87ed4f35e194d6ab602
Immortalis202 <lmalegori@gmail.com>
2024-10-16 15:55:58 +0200

    something

--- src/main/java/com/museum/gestionale/Navigator.java
+++ src/main/java/com/museum/gestionale/Navigator.java
@@ -1,5 +1,9 @@
 package com.museum.gestionale;
 
+import com.museum.gestionale.DbManager.AutoreDao;
+import com.museum.gestionale.DbManager.OperaDao;
+import com.museum.gestionale.Entity.Opera;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,49 +12,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Controller
 public class Navigator {
 
-    @RequestMapping()
-    public String landingPage() {
-        return "index";
-    }
+	@Autowired
+	private AutoreDao autoreDao;
 
-    /*@GetMapping("/fragments")
-    public String getNavbar() {
-        return "navbar";
-    }*/
+	@Autowired
+	private OperaDao operaDao;
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-    @RequestMapping("/info")
-    public String operePage() {
-        return "info";
-    }
+	@RequestMapping()
+	public String landingPage() {
+		return "index";
+	}
+
+	@GetMapping("/")
+	public String index() {
+
+		return "index";
+	}
+
+	@RequestMapping("/info")
+	public String operePage() {
+		return "info";
+	}
 
 
-    @RequestMapping("/form")
-    public String showForm() {
-        return "form";
-    }
+	@RequestMapping("/form")
+	public String showForm() {
+		return "form";
+	}
 
-    @PostMapping("/submitPainting")
-    public String submitPainting(@RequestParam String name,
-                                 @RequestParam String author,
-                                 @RequestParam String date,
-                                 @RequestParam String description,
-                                 @RequestParam String position,
-                                 @RequestParam String originalMuseum,
-                                 @RequestParam String material,
-                                 @RequestParam double height,
-                                 @RequestParam double width,
-                                 @RequestParam MultipartFile image,
-                                 Model model) {
+	@PostMapping("/submitPainting")
+	public String submitPainting(
+			@RequestParam String name,
+			@RequestParam String author,
+			@RequestParam String date,
+			@RequestParam String description,
+			@RequestParam String position,
+			@RequestParam String originalMuseum,
+			@RequestParam String material,
+			@RequestParam double height,
+			@RequestParam double width,
+			@RequestParam MultipartFile image,
+			Model model
+	) {
 
-        model.addAttribute("message", "Painting submitted successfully!");
-        return "result";
-    }
+		model.addAttribute("message", "Painting submitted successfully!");
+		return "result";
+	}
 
 }
